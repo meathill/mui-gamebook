ef43cb2436eebb575bd4fdb676b2d931bc56e092
Meathill <meathill@gmail.com>
2025-11-22 16:28:29 +0800

    change: migrated to OpenNext

--- packages/app/eslint.config.mjs
+++ packages/app/eslint.config.mjs
@@ -1,17 +1,25 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
   // Override default ignores of eslint-config-next.
+  {
+    rules: {
+      'react-hooks/set-state-in-effect': 'off',
+      'quotes': [ 'error', 'single' ],
+      'object-curly-spacing': [ 'error', 'always' ],
+      'computed-property-spacing': [ 'error', 'always' ],
+    },
+  },
   globalIgnores([
     // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
   ]),
 ]);
 
