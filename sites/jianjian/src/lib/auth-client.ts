f86c06ee8f3a0f9a6c60b44c17014a6b896ee6f3
Meathill <meathill@gmail.com>
2025-12-20 20:47:40 +0800

    bug: fixed signing in

--- sites/jianjian/src/lib/auth-client.ts
+++ sites/jianjian/src/lib/auth-client.ts
@@ -1,9 +1,7 @@
 import { createAuthClient } from 'better-auth/react';
-import { getCloudflareContext } from '@opennextjs/cloudflare';
 
 export const authClient = await (async function () {
-  const { env } = await getCloudflareContext({ async: true });
   return createAuthClient({
-    baseURL: env.NEXT_PUBLIC_API_URL,
+    baseURL: process.env.NEXT_PUBLIC_API_URL,
   });
 })();
